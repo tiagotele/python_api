bbee0d980d2998345199f34a6773c8e8660d08a0
Tiago Melo <tiagotele@gmail.com>
2022-05-28 12:18:55 -0300

    Monitoring stack

--- app/api.py
+++ app/api.py
@@ -1,7 +1,15 @@
 from fastapi import FastAPI
+from prometheus_fastapi_instrumentator import Instrumentator
+from prometheus_client import Counter
 
 app = FastAPI()
+Instrumentator().instrument(app).expose(app)
 
+about_counter = Counter(
+    "about_call",
+    "Number of times about endpoint was called.",
+    labelnames=("about",)
+)
 
 @app.get("/")
 async def root():
@@ -9,4 +17,5 @@ async def root():
 
 @app.get("/about")
 async def root():
+    about_counter.labels('about').inc()
     return {"About": "Unifametro class"}
